2f8ff7de5fa7a3da387c575435523e4eb9a14e42
tpi11nfa <tpi11nfa@student.lu.se>
2015-10-01 16:39:03 +0200

    further changes to test, attempts on rosenbrock

--- Optimization_problem.py
+++ Optimization_problem.py
@@ -1,5 +1,7 @@
-
+#defines an optimization problem from a function along with its dimensions and possibly its gradient as well as a decorator in case the user wants to solve another problem than minimization
 class Optimization_problem(object):
-    def __init__(self, fun, grad = None, dec = lambda f: f):
+    def __init__(self, fun, dim, grad = None, dec = lambda f: f):
         self.fun = fun
+        self.dim = dim
+        self.grad = grad
         self.dec = dec
