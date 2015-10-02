63b13e3b8244e2663df3c32f1f448160b4c8981c
DaSimo <simon_reisser@freenet.de>
2015-10-02 09:18:25 +0200

    merge

--- Newton_method.py
+++ Newton_method.py
@@ -5,17 +5,19 @@ class Newton_method(Optimization_method):
     
     def alpha(x_k):
         # setting alpha to 1 as no line search is used
-        return no_line_search(x_k)
+        return no_line_search()
     
-    def s(x_k,h=1.e-5):
+    def s(x_k, h = 1.e-5):
         # computing the inverse Hessian applied to the gradient 
         G = array(eye(Optimization_method.dim))
         # computing the Hessian using finite differenzes on the gradient function
         for i in range(p.dim):
-            unit=array(zeroes(p.dim,1))
-            unit[i]=1
-            G[:,i]=(p.grad(x_k+h*unit)-p.grad(x_k))/h
+            unit = array(zeroes(p.dim,1))
+            unit[i] = 1
+            G[:,i] = (p.grad(x_k+h*unit) - p.grad(x_k)) / h
            # fij≐(f(x1,x2,…,xi+h,…,xj+k,…,xn)−f(x1,x2,…,xi+h,…,xj,…,xn)−f(x1,x2,…,xi,…,xj+k,…,xn)+f(x1,x2,…,xi,…,xj,…,xn))/(hk)
+        #Summetrizing the Hessian
+        G = 0.5 (G + G.T)
         # Computing L with the cholesky method to compute the inverse Hessian
         # If G is not postitv definit this will raise a LinAlgError
         L = chololesky(G)
