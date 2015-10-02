34ebd6def47914136bb7f065ba1380e61ebb343a
Daniel Odenbrand <wishing@live.se>
2015-10-02 16:11:47 +0200

    bugfix, namechanges

--- Good_broyden_method.py
+++ Good_broyden_method.py
@@ -1,25 +1,27 @@
-from Optimization_method import *
+from Quasi_newton_method import *
 
-class Good_Broyden_method(Optimization_method):
+class Good_broyden_method(Quasi_newton_method):
     
-    A_k_1 = array([])
-    g_k_1 = array([])
+    def __init__(self, prob, accuracy):
+        super(self.__class__, self).__init__(prob, accuracy)
+        self.A_k_1 = np.array([])
+        self.g_k_1 = np.array([])
     
     def s(self, x_k, x_k_1=None):
         if x_k_1 == None:
             # Initializing the Matrix and the gradient
-            A_k_1 = eye(len(x_k)) 
-            g_k_1 = p.grad(x_k)
-            return -1* A_k_1
+            self.A_k_1 = np.eye(len(x_k)) 
+            self.g_k_1 = self.p.grad(x_k)
+            return -1* self.A_k_1
         else:
             #Sherman Morrison formula
-            temp = p.grad(x_k)
-            gamma = temp - g_k_1
-            g_k_1 = temp # Saving the evaluation of the gradient for the next step
+            temp = self.p.grad(x_k)
+            gamma = temp - self.g_k_1
+            self.g_k_1 = temp # Saving the evaluation of the gradient for the next step
             delta = x_k - x_k_1
-            u = delta - A_k_1 * gamma
+            u = delta - self.A_k_1 * gamma
             a = 1 / np.dot(u,gamma)
-            A_k = A_k_1 - a*u*u[:,None] # u[:,None] ergiebt u transponiert
-            A_k_1 = A_k # Saving the calculated matrix for the next step
+            A_k = self.A_k_1 - a*u*u[:,None] # u[:,None] ergiebt u transponiert
+            self.A_k_1 = A_k # Saving the calculated matrix for the next step
             return -1*A_k
             
