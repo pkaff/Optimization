52a952c24c9d280e14096f2db413843eb0df94d4
Daniel Odenbrand <wishing@live.se>
2015-10-05 12:21:47 +0200

    line search type now possible to chose

--- Optimization_method.py
+++ Optimization_method.py
@@ -7,10 +7,15 @@ import scipy.spatial.distance as sd
 #Abstract class Optimization_method which can be inherited from. Has a solve method which is general for all newton-methods.
 class Optimization_method(object):
     #Takes a Optimization_problem object and an accuracy (stop condition: when update is smaller than accuracy)
-    def __init__(self, prob, accuracy):
+    #LST = 0 -> no line search
+    #LST = 1 -> exact line search
+    #LST = 2 -> Goldstein inexact line search
+    #LST = 3 -> WP inexact line search
+    def __init__(self, prob, accuracy, lst):
         self.p = prob
         self.acc = accuracy
         self.solve = prob.dec(self.solve)
+        self.line_search_type = lst
 
     def solve(self, f):
         self.p.fun = f
@@ -18,7 +23,7 @@ class Optimization_method(object):
         s_k = self.s(x_pre)
         a = self.alpha(x_pre, s_k)
         x = x_pre + a * s_k
-        if (sd.euclidean(x, 0) > 1.e10):
+        if (sd.euclidean(x, 0) > 1.e10) or (sd.euclidean(f(x), 0) > 1.e10):
             raise Exception('Divergence')
         while sd.euclidean(x, x_pre) > self.acc:
             s_k = self.s(x, x_pre)
@@ -26,15 +31,19 @@ class Optimization_method(object):
             x_temp = x
             x = x + a * s_k
             x_pre = x_temp
-            if (sd.euclidean(x, 0) > 1.e10):
+            if (sd.euclidean(x, 0) > 1.e10) or (sd.euclidean(f(x), 0) > 1.e10):
                 raise Exception('Divergence')
-        return x
+        return [x, f(x)]
 
     def alpha(self, x_k, s_k):
-        #return self.no_line_search()
-        #return self.exact_line_search(x_k, s_k)
-        #return self.WP(x_k, s_k) #inexact line search
-        return self.Goldstein(x_k, s_k) #inexact line search 
+        if self.line_search_type == 0:
+            return self.no_line_search()
+        elif self.line_search_type == 1:
+            return self.exact_line_search(x_k, s_k)
+        elif self.line_search_type == 2:
+            return self.goldstein(x_k, s_k)
+        elif self.line_search_type == 3:
+            return self.WP(x_k, s_k)
 
     def no_line_search(self):
         return 1
@@ -101,7 +110,7 @@ class Optimization_method(object):
     #parameters: x_k and s_k
     #ro, tau and chi are method parameters. Accepted values for ro are in [0, 1/2]
     #aL and aU define the lower and upper bounds for acceptable points.
-    def Goldstein(self, x_k, s_k, ro = 0.1, tau = 0.1, chi = 9., aL = 0, aU = 1.e99):
+    def goldstein(self, x_k, s_k, ro = 0.1, tau = 0.1, chi = 9., aL = 0, aU = 1.e99):
 
         assert 0 <= ro <= 0.5
 
@@ -116,5 +125,5 @@ class Optimization_method(object):
             
         def RC(a):
             return f_a(a) <= (f_a(aL) + ro*(a - aL)* fp_a(aL))
-        
+
         return self.inexact_line_search(x_k, s_k, LC, RC, aL, aU, f_a, fp_a, ro, tau, chi)
--- Test.py
+++ Test.py
@@ -5,72 +5,77 @@ from Bad_broyden_method import *
 from DFP import *
 from BFGS import *
 
+#LST = 0 -> no line search
+#LST = 1 -> exact line search
+#LST = 2 -> Goldstein inexact line search
+#LST = 3 -> WP inexact line search
+
 class TestQuasiNewton(unittest.TestCase):
 
     #tests the newton method with a degree 2 polynomial in 1d
-    @unittest.skip("skip")
+    #@unittest.skip("skip")
     def test_newton_poly2(self):
         expected_minimum = 0.0
         gradient = lambda x: 2.0 * x
         problem = Optimization_problem(self.deg2poly, np.array([2]), gradient)
-        newton = Newton_method(problem, 1.e-8)
+        newton = Newton_method(problem, 1.e-8, 1)
         sol = newton.solve(self.deg2poly)
-        np.testing.assert_array_almost_equal(sol, expected_minimum)
+        np.testing.assert_array_almost_equal(sol[0], expected_minimum)
 
-    @unittest.skip("skip")
+    #@unittest.skip("skip")
     def test_good_broyden_poly2(self):
         expected_minimum = 0.0
         gradient = lambda x: 2.0 * x
         problem = Optimization_problem(self.deg2poly, np.array([2]), gradient)
-        broyden = Good_broyden_method(problem, 1.e-8)
+        broyden = Good_broyden_method(problem, 1.e-8, 1)
         sol = broyden.solve(self.deg2poly)
         np.testing.assert_array_almost_equal(sol, expected_minimum)
         
-    @unittest.skip("skip")
+    #@unittest.skip("skip")
     def test_bad_broyden_poly2(self):
         expected_minimum = 0.0
         gradient = lambda x: 2.0 * x
         problem = Optimization_problem(self.deg2poly, np.array([2]), gradient)
-        broyden = Bad_broyden_method(problem, 1.e-8)
+        broyden = Bad_broyden_method(problem, 1.e-8, 1)
         sol = broyden.solve(self.deg2poly)
-        np.testing.assert_array_almost_equal(sol, expected_minimum)
+        np.testing.assert_array_almost_equal(sol[0], expected_minimum)
 
     def deg2poly(self, x):
         return x**2.0
     
-    @unittest.skip("skip")
-    def test_newton_poly3(self):
+    #@unittest.skip("skip")
+    def test_newton_poly3(self): #should diverge, prints overflow stuff
         expected_minimum = - 4.0/3.0 + np.sqrt(13)/3.0
         gradient = lambda x: 3.0 * x**2.0 + 8.0*x + 1
         problem = Optimization_problem(self.deg3poly, np.array([-5]), gradient)
-        newton = Newton_method(problem, 1.e-8)
+        newton = Newton_method(problem, 1.e-8, 1)
         try:
             sol = newton.solve(self.deg3poly)
             print sol
-            np.testing.assert_array_almost_equal(sol, expected_minimum)
+            np.testing.assert_array_almost_equal(sol[0], expected_minimum)
         except Exception as err:
             assert('Divergence' in err.message)
             
     def deg3poly(self, x):
         return x**3.0 + 4*x**2.0 + x + 1
 
-    @unittest.skip("skip")
+    #@unittest.skip("skip")
     def test_newton_poly4(self):
-        expected_minimum = 0.326345
+        expected_minimum = 0.32634546 #wolframalpha approx
         gradient = lambda x: 4.0 * x**3.0 + 15.0*x**2 + 10*x + 5
         problem = Optimization_problem(self.deg4poly, np.array([5]),gradient)
-        newton = Newton_method(problem, 1.e-8)
+        newton = Newton_method(problem, 1.e-8, 1)
         sol = newton.solve(self.deg4poly)
-        np.testing.assert_array_almost_equal(sol, expected_minimum)
+        np.testing.assert_array_almost_equal(sol[0], expected_minimum)
 
-    @unittest.skip("skip")
+    #@unittest.skip("skip")
     def test_good_broyden_poly4(self):
-        expected_minimum = 0.326345
+        expected_minimum = 0.32634546 #wolframalpha approx
         gradient = lambda x: 4.0 * x**3.0 + 15.0*x**2 + 10*x + 5
         problem = Optimization_problem(self.deg4poly, np.array([5]),gradient)
-        broyden = Good_broyden_method(problem, 1.e-8)
+        broyden = Good_broyden_method(problem, 1.e-8, 1)
         sol = broyden.solve(self.deg4poly)
-        np.testing.assert_array_almost_equal(sol, expected_minimum)
+        np.testing.assert_array_almost_equal(sol[0], expected_minimum)
         
     def deg4poly(self, x):
         return x**4 + 5*x**3 + 5*x**2 - 5*x - 6
@@ -82,18 +87,18 @@ class TestQuasiNewton(unittest.TestCase):
         expected_minimum = np.array([1, 1])
         gradient = lambda x: np.array([202*x[0] - 200*x[1] - 2,200*(x[1] - x[0])])
         problem = Optimization_problem(self.rosen, np.array([4, 4]), gradient)
-        newton = Newton_method(problem, 1.e-8)
+        newton = Newton_method(problem, 1.e-8, 1)
         sol = newton.solve(self.rosen)
-        np.testing.assert_array_almost_equal(sol, expected_minimum)
+        np.testing.assert_array_almost_equal(sol[0], expected_minimum)
 
     #@unittest.skip("skip")
     def test_good_broyden_rosen(self):
         expected_minimum = np.array([1, 1])
         gradient = lambda x: np.array([202*x[0] - 200*x[1] - 2,200*(x[1] - x[0])])
         problem = Optimization_problem(self.rosen, np.array([4, 4]), gradient)
-        broyden = Good_broyden_method(problem, 1.e-8)
+        broyden = Good_broyden_method(problem, 1.e-8, 2)
         sol = broyden.solve(self.rosen)
-        np.testing.assert_array_almost_equal(sol, expected_minimum)
+        np.testing.assert_array_almost_equal(sol[0], expected_minimum, 3)
         
     def rosen(self, x):
         return 100*((x[1] - x[0])**2) + (1 - x[0])**2
