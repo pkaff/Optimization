66dc886e68dd348224dea43d7362eef2ce9e0290
DaSimo <simon_reisser@freenet.de>
2015-10-05 09:28:37 +0200

    DFP undd BFGS

--- BFGS.py
+++ BFGS.py
@@ -1,30 +1,24 @@
 from Quasi_newton_method import *
 
-class Good_broyden_method(Quasi_newton_method):
+class BFGS(Quasi_newton_method):
     
     def __init__(self, prob, accuracy):
         super(self.__class__, self).__init__(prob, accuracy)
-        self.A_k_1 = None
+        self.H_k_1 = None
         self.g_k_1 = None
     
     def s(self, x_k, x_k_1=None):
-        print("x_k: ", x_k, x_k_1)
         if x_k_1 == None:
             # Initializing the Matrix and the gradient
-            print("hello")
             self.A_k_1 = np.eye(len(x_k)) 
             self.g_k_1 = self.p.grad(x_k)
             return -1* self.g_k_1
         else:
-            print("hello2")
-            #Sherman Morrison formula
             temp = self.p.grad(x_k)
             gamma = temp - self.g_k_1
             self.g_k_1 = temp # Saving the evaluation of the gradient for the next step
             delta = x_k - x_k_1
-            u = delta - np.dot(self.A_k_1, gamma)
-            a = 1 / np.dot(u,gamma)
-            A_k = self.A_k_1 - a*u*u # u[:,None] gives u transposed
-            self.A_k_1 = A_k # Saving the calculated matrix for the next step
-            return -1*np.dot(A_k, self.g_k_1)
-
+            sum_1 = (1+np.dot(gamma,self.H_k_1*gamma)/np.dot(delta,gamma))*(np.dot(delta,delta.T)/np.dot(delta, gamma))
+            sum_2 = (np.dot(delta,gamma.T)*self.H_k_1+self.H_k_1*np.dot(gamma,delta.T))/np.dot(delta,gamma)
+            H = self.H_k_1 + sum_1 - sum_2
+            return -1 * H*self.g_k_1
